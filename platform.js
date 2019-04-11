f53c842014cbafe8d2c4b2e24f7fe4f144bbba41
Hydra70 <42453477+Hydra70@users.noreply.github.com>
2019-04-11 12:26:06 -1000

    Add files via upload

--- platform.js
+++ platform.js
@@ -3,8 +3,8 @@ function platform(){
         this.y = 500;
         
         this.show = function(){
-          fill(color('#2b65ec'));
-          rect(this.x,this.y,100,50);
+          fill(color('#848482'));
+          rect(this.x,this.y,600,130);
           strokeWeight(0);
         }
         
